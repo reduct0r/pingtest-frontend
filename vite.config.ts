9ba229ff89da5714cceb5a4e523465b322dff61f
Timur <timurhay1@gmail.com>
2025-11-18 02:22:04 +0300

    add pwa dep

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import mkcert from 'vite-plugin-mkcert';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
+  base: '/pingtest-frontend/',
   server: {
     port: 3000,
     proxy: {
@@ -9,9 +14,32 @@ export default defineConfig({
         target: 'http://localhost:8081',
         changeOrigin: true,
         secure: false,
-    
       }
-    }
+    },
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert.crt')),
+    },
   },
-  plugins: [react()]
+  plugins: [
+    react(),
+    mkcert(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      manifest: {
+        name: 'PINGTEST',
+        short_name: 'PINGTEST',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#fdfdfd',
+        theme_color: '#db4938',
+        orientation: 'portrait-primary',
+        icons: [
+          { src: '/logo192.png', type: 'image/png', sizes: '192x192' },
+          { src: '/logo512.png', type: 'image/png', sizes: '512x512' },
+        ],
+      },
+    }),
+  ],
 });
