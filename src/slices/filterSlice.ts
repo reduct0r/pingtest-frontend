625b7169cde9005ea33f3250eb45737aea77a4e6
Timur <timurhay1@gmail.com>
2025-11-18 01:45:27 +0300

    add filterSlice

---
+++ src/slices/filterSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice} from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+interface FilterState {
+  value: string;
+}
+
+const initialState: FilterState = {
+  value: '',
+};
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setFilter: (state, action: PayloadAction<string>) => {
+      state.value = action.payload;
+    },
+    clearFilter: (state) => {
+      state.value = '';
+    },
+  },
+});
+
+export const { setFilter, clearFilter } = filterSlice.actions;
+export default filterSlice.reducer;
