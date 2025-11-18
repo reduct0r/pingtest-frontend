cbb33cd6a71a2485db66b71b8592ebf73ef31737
Timur <timurhay1@gmail.com>
2025-11-19 02:36:40 +0300

    tauri

--- vite.config.ts
+++ vite.config.ts
@@ -5,46 +5,51 @@ import mkcert from 'vite-plugin-mkcert';
 import fs from 'fs';
 import path from 'path';
 
-export default defineConfig({
-  base: '/pingtest-frontend/',
-  server: {
-    host: true,
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8081',
-        changeOrigin: true,
-        secure: false,
-      }
-    },
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert.crt')),
-    },
-  },
-  plugins: [
-    react(),
-    mkcert(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: { enabled: false },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,png,svg,jpg,gif,ico}'],
+export default defineConfig(() => {
+  const isTauri = process.env.TAURI_PLATFORM !== undefined;
+  const base = isTauri ? './' : '/pingtest-frontend/';
+
+  return {
+    base,
+    server: {
+      host: true,
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: 'http://192.168.15.7:8081',
+          changeOrigin: true,
+          secure: false,
+        }
       },
-      manifestFilename: 'manifest.json',
-      manifest: {
-        name: 'PINGTEST',
-        short_name: 'PINGTEST',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#fdfdfd',
-        theme_color: '#db4938',
-        orientation: 'portrait-primary',
-        icons: [
-          { src: '/logo192.png', type: 'image/png', sizes: '192x192' },
-          { src: '/logo512.png', type: 'image/png', sizes: '512x512' },
-        ],
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'cert.crt')),
       },
-    }),
-  ],
+    },
+    plugins: [
+      react(),
+      mkcert(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: { enabled: false },
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,png,svg,jpg,gif,ico}'],
+        },
+        manifestFilename: 'manifest.json',
+        manifest: {
+          name: 'PINGTEST',
+          short_name: 'PINGTEST',
+          start_url: '/',
+          display: 'standalone',
+          background_color: '#fdfdfd',
+          theme_color: '#db4938',
+          orientation: 'portrait-primary',
+          icons: [
+            { src: '/logo192.png', type: 'image/png', sizes: '192x192' },
+            { src: '/logo512.png', type: 'image/png', sizes: '512x512' },
+          ],
+        },
+      }),
+    ],
+  };
 });
